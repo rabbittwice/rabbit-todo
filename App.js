b436fb6dfd5383735041941ee5f7166608948cfa
labtopSky <2skyer@naver.com>
2019-12-31 15:48:58 +0900

    togglestyle of ToDo

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import ToDo from './ToDo';
 
 const { height, width } = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content"/>
-      <Text style={styles.title}>Rabbit To Do</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"}></TextInput>
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+  render () {
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.title}>Rabbit To Do</Text>
+        <View style={styles.card}>
+          <TextInput 
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo} 
+            onChangeText={this._controllNewToDo}
+            placeholderTextColor={"#999"} 
+            returnKeyType={"done"} 
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos} >
+            <ToDo/>
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  _controllNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -49,5 +72,14 @@ const styles = StyleSheet.create({
         elevation: 10
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos: {
+    alignItems: 'center'
   }
 });
